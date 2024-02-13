57200cc88628790842e42c06828c469e7301357f
Lyudmyla Prischepa <lgavlich@gmail.com>
2024-02-13 21:06:32 +0200

    main root

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { createContext, useState } from 'react';
 import { NavBar, Banner, Experience, Skills, Contact, Footer, Project } from './components';
-import './App.css';
+// import './App.css';
+import styles from './App.module.css';
 export const ThemeContext = createContext(null);
 
 function App() {
@@ -10,6 +11,9 @@ function App() {
   };
   return (
     <div style={{ backgroundColor: '#823ae04d' }}>
+      <div className={styles.bgHero}>
+        <div className={styles.leftSide} />
+      </div>
       <ThemeContext.Provider value={{ setHandlerDrawer }}>
         <NavBar />
         <Banner />
